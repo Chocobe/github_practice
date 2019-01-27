09599e0e7df22da470574728a47ae2f7c1932d7c
Chocobe <kyw05171@gmail.com>
2019-01-27 15:22:52 +0900

    <master> : Create files "Github_push.java"

---
+++ github_push/Github_push.java
@@ -0,0 +1,9 @@
+package github_push;
+
+public class Github_push {
+	public static void main(String[] args) {
+		System.out.println("----------------");
+		System.out.println("Github push Test");
+		System.out.println("-               -");
+	}
+}
