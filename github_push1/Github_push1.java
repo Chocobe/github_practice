0dc1164b85c03c2c1b5166086c1e12a4a6f47ce2
Chocobe <kyw05171@gmail.com>
2019-01-27 15:35:50 +0900

    <push1> : Added output "Tell your World"

--- github_push1/Github_push1.java
+++ github_push1/Github_push1.java
@@ -4,6 +4,7 @@ public class Github_push1 {
 	public static void main(String[] args) {
 		System.out.println("------------------");
 		System.out.println("Github push test_1");
-		System.out.println("- - - - - - - - - ");
+		System.out.println("- - - - - - - - - \n");
+		System.out.println("Tell your World");
 	}
 }
