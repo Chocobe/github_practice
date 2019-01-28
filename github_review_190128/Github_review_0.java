87e11b9f4b157bc1428de7d29e9f8d4b332de906
Chocobe <kyw05171@gmail.com>
2019-01-28 11:03:39 +0900

    <github_review_190128 - master> : Added output "Tell your World"

--- github_review_190128/Github_review_0.java
+++ github_review_190128/Github_review_0.java
@@ -3,5 +3,6 @@ package github_review_190128;
 public class Github_review_0 {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Tell your World");
 	}
 }
