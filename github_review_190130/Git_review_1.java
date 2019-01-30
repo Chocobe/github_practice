6ff7e151d0ee0400b49738bbb670112c3d03fc27
Chocobe <kyw05171@gmail.com>
2019-01-30 10:41:22 +0900

    <github_review_190130 - master> : Created this files

---
+++ github_review_190130/Git_review_1.java
@@ -0,0 +1,8 @@
+package github_review_190130;
+
+public class Git_review_1 {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Tell your World");
+	}
+}
