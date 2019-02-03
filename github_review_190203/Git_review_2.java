4493d23354f042f338633e5a925cec6c6cf70495
Young Woo Kim <kyw05171@gmail.com>
2019-02-03 15:50:17 +0900

    Modified comment on Github repo

--- github_review_190203/Git_review_2.java
+++ github_review_190203/Git_review_2.java
@@ -1,5 +1,6 @@
 public class Git_review_2 {
 	public static void main(String[] args) {
+		// Modified comment on Github repo
 		System.out.println("Hello Github");
 		System.out.println("Tell your World");
 	}
